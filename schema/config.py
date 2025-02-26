b99d9a6e07f69e611ef7a54b1dcb406a8c05594e
Andrew R. Boeckh <60844959+arboeckh@users.noreply.github.com>
2025-02-26 09:56:23 -0500

    Update config.py

--- schema/config.py
+++ schema/config.py
@@ -10,7 +10,7 @@ class Config:
     DB_PASSWORD = os.getenv("DB_PASSWORD", "password")
     DB_HOST = os.getenv("DB_HOST", "localhost")
     DB_PORT = os.getenv("DB_PORT", "5432")
-    DB_NAME = os.getenv("DB_NAME", "devdb")  # Default to dev database
+    DB_NAME = os.getenv("DB_NAME", "dbdev")  # Default to dev database
     DB_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 
